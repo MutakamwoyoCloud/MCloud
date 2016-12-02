1473d13f825683403c2153a3760ae8d501cd8f0b
juanmont <juanmont@ucm.es>
2016-12-02 17:22:27 +0100

    creada pagina principal

--- src/Table.js
+++ src/Table.js
@@ -5,27 +5,36 @@ var Table = require('fixed-data-table').Table;
 var Column = require('fixed-data-table').Column;
 var Cell = require('fixed-data-table').Cell;
 var Button = require('react-foundation').Button;
+//var partitionHandler = require('./core/PetitionHandler');
 
+const rows = [
+  ['a1', 'b1', 'c1'],
+  ['a2', 'b2', 'c2'],
+  ['a3', 'b3', 'c3'],
+  // .... and more
+];
 // Table data as a list of array.
 class TableResult extends Component {
 // Render your table
   constructor(props) {
     super(props);
-    this.rows = props.rows;
+    this.rows = rows;
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
   handleSubmit(event) {
     alert("Pulsada fila: "+event.target["id"]);
+    //console.log(partitionHandler.pruebaDatos());
     event.preventDefault();
   }
   
   render(){
     var numCols = 4;
-    //ar screen = screen.width;
+    //console.log(width)
     var rows = null;
     var length = 0;
+    var width = screen.width - 100;
     if(this.rows){
       rows = this.rows;
       length = rows.length;
@@ -34,7 +43,7 @@ class TableResult extends Component {
       <Table
         rowHeight={50}
         rowsCount={length}
-        width={screen.width}
+        width={width}
         height={50*(length+1)}
         headerHeight={50}>
         <Column
@@ -44,7 +53,7 @@ class TableResult extends Component {
               {rows[rowIndex][0]}
             </Cell>
           )}
-          width={screen.width/numCols}
+          width={width/numCols}
         />
         <Column
           header={<Cell>Col B</Cell>}
@@ -53,7 +62,7 @@ class TableResult extends Component {
               {rows[rowIndex][1]}
             </Cell>
           )}
-          width={screen.width/numCols}
+          width={width/numCols}
         />
         <Column
           header={<Cell>Col C</Cell>}
@@ -62,7 +71,7 @@ class TableResult extends Component {
               {rows[rowIndex][2]}
             </Cell>
           )}
-          width={screen.width/numCols}
+          width={width/numCols}
         />
         <Column
           header={<Cell>Col C</Cell>}
@@ -73,7 +82,7 @@ class TableResult extends Component {
               </form>
             </Cell>
           )}
-          width={screen.width/numCols}
+          width={width/numCols}
           align={'center'}
         />
       </Table>
