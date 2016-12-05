b3fc7a2a3c312601ee5d5e1a302417d574f2dc9b
semedi <sergio.semedi@gmail.com>
2016-12-05 12:18:39 +0100

    testing node server with sql

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NameForm from './NameForm';
 import Table from './Table';
 //import './App.css';
-import '../node_modules/react-foundation/node_modules/foundation-sites/dist/foundation.min.css'
+import '../node_modules/foundation-sites/dist/css/foundation.min.css'
 import '../node_modules/fixed-data-table/dist/fixed-data-table.min.css'
 
 require('react-foundation');
