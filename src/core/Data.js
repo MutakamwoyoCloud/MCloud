ea47b9e128741cc844c571062dd03f0944ab3ee9
Sergio <sergio.semedi@gmail.com>
2017-02-28 17:53:08 +0100

    adding operations to data handler

--- src/core/Data.js
+++ src/core/Data.js
@@ -1,12 +1,16 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
+
 var url = 'mongodb://localhost:27017/mcloud';
+var u = 'mongodb://localhost:5223/mcloud';
+
 
 
 var Data = module.exports = function(server){
 
-    var final_direction = url;
+
+    var final_direction = u;
 
     if (server)
         final_direction = server;
@@ -15,22 +19,28 @@ var Data = module.exports = function(server){
     MongoClient.connect(final_direction, function(err, db) {
         assert.equal(null, err);
         console.log("Connecting to data model...");
-        
+
         //db.createCollection('test', function(err, collection) {});
 
         db.close();
     });
 };
 
+var operation = module.exports = {
+    insert: function(){
+        var collection = db.collection('main');
+
+    }
+}
+
 
 Data.prototype.insert= function(toInstert){
 
-
         MongoClient.connect(final_direction, function(err, db) {
         assert.equal(null, err);
-        
+
              db.collection('main').insertOne( toInsert
-                , 
+                ,
                 function(err, result) {
                     assert.equal(err, null);
                 });
@@ -44,3 +54,4 @@ Data.prototype.insert= function(toInstert){
 
 
 var data = new Data();
+
