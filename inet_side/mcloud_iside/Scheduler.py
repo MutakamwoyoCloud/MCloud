f7a4889e5179b5e17593d3910510c0d7e29a5380
juanmont <juanmont@ucm.es>
2017-03-03 17:24:48 +0100

    ordenando por prioridad

--- inet_side/mcloud_iside/Scheduler.py
+++ inet_side/mcloud_iside/Scheduler.py
@@ -3,6 +3,11 @@ from utils.utils import decompress
 from utils.utils import compress
 from wiki import Wiki
 import os
+priority = {
+    "wiki":0,
+    "vademecum":1,
+    "youtube":2
+}
 
 class Scheduler:
     def __init__(self):
@@ -10,8 +15,17 @@ class Scheduler:
         self.wiki = Wiki()
         self.path = os.getcwd()
 
+    def splitPath(self,petition):
+        names = petition.split("/")
+        names = names[len(names)-1].split(".")
+        return names
+
     def enqueue(self, x):
-        self.items.append(x)
+        print x
+        name = self.splitPath(x)[0]
+        splitPetition = name.split("_")
+        typePetition = splitPetition[len(splitPetition)-1]
+        self.insertOrderListPetition(priority[typePetition], x)
 
     def dequeue(self):
         try:
@@ -24,8 +38,7 @@ class Scheduler:
 
     def process(self):
         petition = self.dequeue()
-        names = petition.split("/")
-        names = names[len(names)-1].split(".")
+        names = self.splitPath(petition)
         list = decompress(names[0], petition)
         listOut = [];
         os.mkdir(list['dir']+"/result/");
@@ -36,4 +49,30 @@ class Scheduler:
         print listOut
         compress(listOut,list['dir']+"/result/", list['dir']+"/"+names[0]+"_out.tar.gz")
         os.chdir(self.path)
+
+    def insertOrderListPetition(self, num, petition):
+        i = 0
+        if(len(self.items)>0):
+            name = self.splitPath(self.items[i])[0]
+            splitPetition = name.split("_")
+            typePetition = splitPetition[len(splitPetition)-1]
+            numType = priority[typePetition]
+            i+=1
+            while i < len(self.items) and numType <= num:
+                name = self.splitPath(self.items[i])[0]
+                splitPetition = name.split("_")
+                typePetition = splitPetition[len(splitPetition)-1]
+                numType = priority[typePetition]
+                i+=1
+
+            lenA = len(self.items)
+            for j, val in self.items:
+                self.items.insert(lenA-j, self.items[lenA-j-1])
+
+        self.items.insert(i, petition)
+
+            
+
+
+    
 
