5118e396e169b49e3534bab8a7f2f5562267b663
Alba <albissweet90@gmail.com>
2017-05-04 17:37:46 +0200

    Main view styled

--- src/routes.js
+++ src/routes.js
@@ -2,27 +2,19 @@
 import React from 'react';
 import { Router, Route , browserHistory} from 'react-router';
 
-
-//import About from './components/About';
 import NotFound from './components/NotFound';
-//import home from './HomeAux';
 import Principal from './Principal';
 import Peticiones from './components/Peticiones';
 import Correo from './components/Correo';
-import Vademecum from './components/Vademecum';
-import Youtube from './components/Youtube';
 import Result from './components/Result';
-//import PrincipalAux from './PrincipalAux';
-//import {Correo} from './components/Correo';
 
 const Routes = (props) => (
   
-  //const Routes = () =>(
 	  <Router history={browserHistory}>
 	    <Route path="/" component={Principal} />
 	    <Route path="/peticiones" name='Wikipedia' component={Peticiones} />
-	    <Route path="/vademecum" name='Vademecum' component={Vademecum} />
-	    <Route path="/youtube" name='Youtube' component={Youtube} />
+	    <Route path="/vademecum" name='Vademecum' component={Peticiones} />
+	    <Route path="/youtube" name='Youtube' component={Peticiones} />
 	    <Route path="/correo" name='Correo' component={Correo} />
 	    <Route path="/result" name='Resultado' component={Result} />
 	    <Route path="*" component={NotFound} />
