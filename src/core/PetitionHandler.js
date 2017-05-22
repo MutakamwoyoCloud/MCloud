1963f3a82efd9b2c4d42b17fafcad9122e77069c
juanmont <juanmont@ucm.es>
2017-05-22 19:37:37 +0200

    arreglados errores de estilo en el codigo

--- src/core/PetitionHandler.js
+++ src/core/PetitionHandler.js
@@ -37,10 +37,99 @@ schedule.scheduleJob(time, function(){
   }, 1000*60*60);
 });
 schedule.scheduleJob(timeFinish, function(){
-  if(executeJobTime != ""){
+  if(executeJobTime !== ""){
     clearTimeout(executeJobTime);
   }
 });
+
+//this function create a package and enqueue
+function create_package(self, type){
+
+  self.emitter.on("newPackage", function(id){
+
+    var archive = archiver("tar", {
+      gzip: true,
+      store: true // Sets the compression method to STORE.
+    });
+
+    // good practice to catch this error explicitly
+    archive.on("error", function(err) {
+      throw err;
+    });
+
+    var output = fs.createWriteStream(__dirname + "/push/"+id+"_"+type+".tar.gz");
+
+    // listen for all archive data to be written
+    output.on("close", function() {
+
+      console.log(archive.pointer() + " total bytes");
+      console.log("archiver has been finalized and the output file descriptor has closed.");
+    });
+
+    archive.pipe(output);
+
+    self.enqueue(id);
+
+    for (var i = 0, len = self._petitionsObj[type].length; i < len; i++){
+      archive.append(JSON.stringify(self._petitionsObj[type][i]), {name: i+".json"});
+    }
+    archive.finalize();
+    self.reset(type);
+  });
+
+self.data.do(_model.op.insert,{}, {ready: false}, self.emitter);
+
+}
+
+//receive and decompress a package 
+function receive_package(self){
+  var pull_folder = pullFolder;
+  fs.readdir(pull_folder, (err, files) => {
+    files.forEach(file => {
+      decompress(pull_folder+file, pull_folder+"dist_"+file.split("_")[0]).then(files_decompress => {
+        files_decompress.forEach(file_decompress => {
+          var type = file_decompress.path.split("_")[0];
+          var name_search = file_decompress.path.split("_")[1].split(".")[0];
+          var json = require(__dirname+"/pull/dist_"+file.split("_")[0]+"/"+file_decompress.path);
+          var json_insert = {};
+          json_insert["name"] = json["name"];
+          json_insert["data"] = json;
+          console.log("insertData");
+          self.data.do(_model.op.insertData,{}, json_insert, self.emitter, 1);
+        });
+      });
+      self.emitter.on("pulled", function(){
+        fs.appendFile(__dirname+"/flush/remove.txt", file+"\n", function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        }); 
+        if( fs.existsSync(__dirname+"/pull/"+file) ) {
+          deleteFolderRecursive(__dirname+"/pull/dist_"+file.split("_")[0]);
+        }
+        if( fs.existsSync(__dirname+"/pull/"+file) ) {
+          self.data.do(_model.op.remove,file.split("_")[0], {}, null);
+          fs.unlinkSync(__dirname+"/pull/"+file);
+        }
+      })
+    });
+  });
+}
+
+function deleteFolderRecursive(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
 
 var PH = module.exports = function(package_size){
 
@@ -77,13 +166,11 @@ PH.prototype.enqueue = function(data) {
 };
 
 PH.prototype.dequeue = function(){
-  var oldIdx = this._qoldidx,
-      newIdx = this._qnewidx,
-      toDelete;
+  var toDelete;
 
-  if (oldIdx !== newIdx){
-    toDelete = this._qstorage[oldIdx];
-    delete this._qstorage[oldIdx];
+  if (this._qoldidx !== this._qnewidx){
+    toDelete = this._qstorage[this._qoldidx];
+    delete this._qstorage[this._qoldidx];
     this._qoldidx++;
 
     return toDelete;
@@ -108,22 +195,26 @@ PH.prototype.add_petition= function(data){
   this._petitionsNum[data.type]++;
   console.log(this._petitionsNum[data.type]);
   console.log(this._package_size);
-  if (this._petitionsNum[data.type] == this._package_size){
+  if (this._petitionsNum[data.type] === this._package_size){
     create_package(this, data.type);
   }
 };
 
 PH.prototype.search= function(callback, data, type){
   this.emitter.once("findSomeone", function(dataFind){
-    if(dataFind)
+    if(dataFind){
       callback(dataFind);
-    else
+    }
+    else{
       callback({});
+    }
   });
-  if(type == "wiki")
+  if(type === "wiki"){
     this.data.do(_model.content.getSome,{}, data, this.emitter, 1);
-  if(type == "youtube")
+  }
+  if(type === "youtube"){
     this.data.do(_model.content.getSome,{}, data, this.emitter, 2);
+  }
 };
 
 PH.prototype.pull= function(callback){
@@ -144,110 +235,6 @@ PH.prototype.searchOne= function(callback, data, type){
     else
       callback({});
   });
-  if(type == "wiki")
+  if(type === "wiki")
     this.data.do(_model.content.get,{}, data, this.emitter, 1);
 };
-
-
-// private
-/**************************************************************************/
-
-var time = {};
-
-
-time.hour = 11;
-time.minute = 57;
-
-schedule.scheduleJob(time, function(){
-  console.log("son las "+ time.hour+":"+time.minute);
-  ftpw.exec(ftpw.action.push);
-});
-
-//this function create a package and enqueue
-function create_package(self, type){
-
-  self.emitter.on("newPackage", function(id){
-
-    var archive = archiver("tar", {
-      gzip: true,
-      store: true // Sets the compression method to STORE.
-    });
-
-    // good practice to catch this error explicitly
-    archive.on("error", function(err) {
-      throw err;
-    });
-
-    var output = fs.createWriteStream(__dirname + "/push/"+id+"_"+type+".tar.gz");
-
-    // listen for all archive data to be written
-    output.on("close", function() {
-
-      console.log(archive.pointer() + " total bytes");
-      console.log("archiver has been finalized and the output file descriptor has closed.");
-    });
-
-    archive.pipe(output);
-
-    self.enqueue(id)
-
-    for (var i = 0, len = self._petitionsObj[type].length; i < len; i++){
-      archive.append(JSON.stringify(self._petitionsObj[type][i]), {name: i+".json"});
-    }
-    archive.finalize();
-    self.reset(type);
-  });
-
-self.data.do(_model.op.insert,{}, {ready: false}, self.emitter);
-
-}
-
-//receive and decompress a package 
-function receive_package(self){
-  var pull_folder = pullFolder;
-  fs.readdir(pull_folder, (err, files) => {
-    files.forEach(file => {
-      decompress(pull_folder+file, pull_folder+"dist_"+file.split("_")[0]).then(files_decompress => {
-        files_decompress.forEach(file_decompress => {
-          var type = file_decompress.path.split("_")[0];
-          var name_search = file_decompress.path.split("_")[1].split(".")[0];
-          var json = require(__dirname+"/pull/dist_"+file.split("_")[0]+"/"+file_decompress.path);
-          var json_insert = {};
-          json_insert["name"] = json["name"];
-          json_insert["data"] = json;
-          console.log("insertData");
-          self.data.do(_model.op.insertData,{}, json_insert, self.emitter, 1);
-        });
-      });
-      self.emitter.on("pulled", function(){
-        fs.appendFile(__dirname+"/flush/remove.txt", file+'\n', function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        }); 
-        if( fs.existsSync(__dirname+"/pull/"+file) ) {
-          deleteFolderRecursive(__dirname+"/pull/dist_"+file.split("_")[0]);
-        }
-        if( fs.existsSync(__dirname+"/pull/"+file) ) {
-          self.data.do(_model.op.remove,file.split("_")[0], {}, null);
-          fs.unlinkSync(__dirname+"/pull/"+file);
-        }
-      })
-    });
-  });
-}
-
-function deleteFolderRecursive(path) {
-  if( fs.existsSync(path) ) {
-    fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
-      if(fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
-}
